c36ad9d85c5892f6770e353e5c4d66c51eb38161
joaofbmaia <22353787+joaofbmaia@users.noreply.github.com>
2023-11-29 01:26:37 +0000

    Add DFG generation and LLVM build steps

--- init_script.sh
+++ init_script.sh
@@ -12,9 +12,22 @@ unzip clava.zip -d kernel_transformer/clava
 # Remove the clava.zip file
 rm clava.zip
 
+
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+git checkout llvmorg-10.0.0
+# Replace benchmark_register.h file
+wget https://github.com/llvm/llvm-project/raw/llvmorg-12.0.0/llvm/utils/benchmark/src/benchmark_register.h -O llvm/utils/benchmark/src/benchmark_register.h
+mkdir llvm-build
+cd llvm-build
+cmake -DLLVM_ENABLE_PROJECTS='polly;clang' -DLLVM_USE_LINKER=gold -DLLVM_TARGETS_TO_BUILD="X86" -G "Unix Makefiles" ../llvm
+make -j$(nproc)
+
+cd ../..
+
 # Build DFG Generator
 cd dfg_generator
 mkdir build
 cd build
 cmake ..
-make VERBOSE=1 -j 4
+make VERBOSE=1 -j$(nproc)
--- run_streamflow.py
+++ run_streamflow.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import os
 import shutil
+import subprocess
 
 if len(sys.argv) < 2:
     print("Please provide the source file path as an argument.")
@@ -30,35 +31,73 @@ flatten_code_dir = source_file_dir + "/flatten_code"
 flatten_code_path = flatten_code_dir + "/" + source_file_name
 clava_morpher_format_path = kernel_transformer_dir + "/morpher_format.js"
 
+morpher_formatted_code_dir = source_file_dir + "/morpher_formatted_code"
+morpher_formatted_code_path = morpher_formatted_code_dir + "/" + source_file_name
 
-# Unroll the code
-command = ["java", "-jar", "Clava.jar", clava_unroll_path, "-p", source_file_path, "-o", source_file_dir, "-of", "unrolled_code"]
+dfg_gen_output_dir = source_file_dir + "/dfg_gen_output"
+ll_ir_path = dfg_gen_output_dir + "/" + source_file_name_without_extension + ".ll"
+
+
+
+# # Unroll the code
+# command = ["java", "-jar", "Clava.jar", clava_unroll_path, "-p", source_file_path, "-o", source_file_dir, "-of", "unrolled_code"]
+
+# try:
+#     subprocess.run(command, check=True, cwd=clava_dir)
+#     print("Clava unroll transformation completed successfully.")
+# except subprocess.CalledProcessError as e:
+#     print("Clava unroll transformation failed:", e)
+
+# # Flatten the code
+# command = ["java", "-jar", "Clava.jar", clava_flatten_path, "-p", unrolled_code_path, "-o", source_file_dir, "-of", "flatten_code"]
+
+# try:
+#     subprocess.run(command, check=True, cwd=clava_dir)
+#     print("Clava flatten transformation completed successfully.")
+# except subprocess.CalledProcessError as e:
+#     print("Clava flatten transformation failed:", e)
+
+# # Format code for Morpher
+# command = ["java", "-jar", "Clava.jar", clava_morpher_format_path, "-p", flatten_code_path, "-o", source_file_dir, "-of", "morpher_formatted_code"]
+
+# try:
+#     subprocess.run(command, check=True, cwd=clava_dir)
+#     print("Clava morpher format transformation completed successfully.")
+# except subprocess.CalledProcessError as e:
+#     print("Clava morpher format transformation failed:", e)
+
+
+# Run Morpher DFG Generator
+
+llvm_bin_dir = streamflow_dir + "/llvm-project/llvm-build/bin"
+clang_bin = llvm_bin_dir + "/clang"
+opt_bin = llvm_bin_dir + "/opt"
+
+# Create dfg_gen_output directory if it doesn't exist
+os.makedirs(dfg_gen_output_dir, exist_ok=True)
+
+
+command = [
+    clang_bin,
+    "-D",
+    "CGRA_COMPILER",
+    "-target",
+    "i386-unknown-linux-gnu",
+    "-c",
+    "-emit-llvm",
+    "-O2",
+    "-fno-tree-vectorize",
+    "-fno-inline",
+    "-fno-unroll-loops",
+    morpher_formatted_code_path,
+    "-S",
+    "-o",
+    ll_ir_path
+]
 
 try:
-    subprocess.run(command, check=True, cwd=clava_dir)
-    print("Clava unroll transformation completed successfully.")
+    subprocess.run(command, check=True)
+    print("clang command executed successfully.")
 except subprocess.CalledProcessError as e:
-    print("Clava unroll transformation failed:", e)
+    print("clang command failed:", e)
 
-# Flatten the code
-command = ["java", "-jar", "Clava.jar", clava_flatten_path, "-p", unrolled_code_path, "-o", source_file_dir, "-of", "flatten_code"]
-
-try:
-    subprocess.run(command, check=True, cwd=clava_dir)
-    print("Clava flatten transformation completed successfully.")
-except subprocess.CalledProcessError as e:
-    print("Clava flatten transformation failed:", e)
-
-# Format code for Morpher
-command = ["java", "-jar", "Clava.jar", clava_morpher_format_path, "-p", flatten_code_path, "-o", source_file_dir, "-of", "morpher_formatted_code"]
-
-try:
-    subprocess.run(command, check=True, cwd=clava_dir)
-    print("Clava morpher format transformation completed successfully.")
-except subprocess.CalledProcessError as e:
-    print("Clava morpher format transformation failed:", e)
-
-
-
-# shutil.move(source_file_dir + "/woven_code/" + source_file_name, os.path.join(source_file_dir, source_file_name_without_extension + "_woven" + source_file_extension))
-# shutil.rmtree(source_file_dir + "/woven_code/")
